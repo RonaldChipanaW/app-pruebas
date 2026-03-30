7d8baf3963026b356cd063507c4407471f4f57ec
kekeHub9 <rebeca9scott@gmail.com>
2026-03-30 16:29:01 -0400

    aca use el git merge para cambiar la rama en develop

---
+++ src/functions/areaCirculo.py
@@ -0,0 +1,13 @@
+def area_circulo(radio):
+    """
+    Calcula el área de un círculo dado su radio.
+    
+    Parámetros:
+    radio (float): El radio del círculo
+    
+    Retorna:
+    float: El área del círculo
+    """
+    pi = 3.1416
+    area = pi * (radio ** 2)
+    return area
